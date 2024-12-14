c181031b00ebbd09208d5953fdddc2c12f062388
Seth Hoenig <shoenig@duck.com>
2024-12-14 07:48:51 -0600

    nerfs: able to scan text against artifact domain and word block lists

--- nerfs_test.go
+++ nerfs_test.go
@@ -4,8 +4,11 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strings"
 	"testing"
 
+	"github.com/hashicorp/go-set/v3"
 	"github.com/shoenig/test/must"
 )
 
@@ -61,4 +64,51 @@ func TestArtifact_Load(t *testing.T) {
 	must.NoError(t, lerr)
 	must.Size(t, 3, art.domains)
 	must.Len(t, 2, art.words)
+}
+
+func TestSynopsis_matching(t *testing.T) {
+	t.Parallel()
+
+	a := &Artifact{
+		domains: set.From([]string{"example.com", "example.org", "example.xyz"}),
+		words: []*regexp.Regexp{
+			regexp.MustCompile(`p[oO0]+p`),
+			regexp.MustCompile(`f[a4]rt`),
+		},
+	}
+
+	t.Run("no match", func(t *testing.T) {
+		r := strings.NewReader("this is some text! wow!")
+		syn := a.Synopsis(r)
+		must.Zero(t, syn.Domains)
+		must.Zero(t, syn.Words)
+		must.False(t, syn.Any())
+	})
+
+	t.Run("block domain", func(t *testing.T) {
+		r := strings.NewReader("visit example.org today! wow!")
+		syn := a.Synopsis(r)
+		must.One(t, syn.Domains)
+		must.Zero(t, syn.Words)
+		must.True(t, syn.Any())
+	})
+
+	t.Run("block word", func(t *testing.T) {
+		r := strings.NewReader("take a big p00p! wow!")
+		syn := a.Synopsis(r)
+		must.Zero(t, syn.Domains)
+		must.One(t, syn.Words)
+		must.True(t, syn.Any())
+	})
+
+	t.Run("block mix", func(t *testing.T) {
+		text := `
+visit example.org we have pOop and f4rts! example.xyz and example.com too!
+`
+		r := strings.NewReader(text)
+		syn := a.Synopsis(r)
+		must.Eq(t, 3, syn.Domains)
+		must.Eq(t, 2, syn.Words)
+		must.True(t, syn.Any())
+	})
 }
